90877f8fef1b162ce8a30c8b174bb8f59c64cfa6
chandaku <gawri.chandan@gmail.com>
2022-10-02 14:16:34 +0530

    removed deperecated method call

---
+++ Readme.md
@@ -0,0 +1,3 @@
+### Configuration Loaded are build from pure-config
+
+https://pureconfig.github.io/v0.12.0/docs/loading-a-config.html
--- src/main/scala/com/learnscala/main/App.scala
+++ src/main/scala/com/learnscala/main/App.scala
@@ -1,8 +1,6 @@
 package com.learnscala.main
 
 import pureconfig._
-import pureconfig.generic.auto._
-
 import scala.reflect.ClassTag
 
 abstract class App [T:ClassTag:ConfigReader] {
@@ -10,7 +8,7 @@ abstract class App [T:ClassTag:ConfigReader] {
   protected val settings : T = loadSettings
 
   protected def loadSettings: T = {
-    loadConfigOrThrow[T]
+    ConfigSource.default.loadOrThrow[T]
   }
 
   def getSetting() : T = {
